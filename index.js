8f5372a72af0f4af935ab7a4dfc0e7314e1623eb
Bryan Jazo <bryanjazo88@gmail.com>
2021-05-01 12:47:22 -0700

    commit tree a011f79285de6116b6f296b21a8f43fdf97f5ab8

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function displayMessage(display) {
+ let doc =  document.getElementById('greeting')
+ doc.innerText = display;
+}
+
 /* Write your implementation of greet() */
+function greet(time) {
+
+
+
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
